5ce22f68ac3754ac8bc698bea7c04cd5e81b0b57
marco.burrometo@gmail.com <marco.burrometo@gmail.com>
2022-02-23 13:14:52 +0100

    23-02

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,16 +1,37 @@
+import { listaTaks } from '../../App';
 import Card from '../Card/Card';
 import './Task.scss';
 import immagine from './trashcan.png';
 
-function Task({ id, descrizione, data, fatto }) {
+function Task({ id, descrizione, data, fatto, onDelete, onComplete }) {
+    var className = 'descrizione';
+    if (fatto) {
+        className += ' fatto';
+    }
+
+    const renderCheckbox = () => {
+        if (fatto) {
+            return <span className="check">✅</span>;
+        }
+        return null;
+    }
+
+    const checkbox = fatto ? <span className="check">✅</span> : null;
+
     return (
         <Card className="task">
             <div className='left-area'>
                 <span className="data">{data.toString().substring(4, 21)}</span>
-                <span className="descrizione">{descrizione}</span>
+                <span className={className}>{descrizione}</span>
+                {renderCheckbox()}
             </div>
             <div className="right-area">
-                <img alt="delete" src={immagine} />
+                <input type="checkbox" checked={fatto} onChange={() => {
+                    onComplete(id, !fatto)
+                }} />
+                <img alt="delete" src={immagine} onClick={() => {
+                    onDelete(id);
+                }} />
             </div>
         </Card >
 
